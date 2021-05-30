5e77f3f0900eb1eecfdb82d1af41dcfab258a738
Taiki Shiraishi <operatic-earthquake+github@pm.me>
2021-05-31 00:07:02 +0900

    refactor: button css to scss

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -1,21 +1,15 @@
-import './button.css';
+import '@assets/stylesheets/main.scss'
 
-export const createButton = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  onClick,
-}) => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = label;
-  btn.addEventListener('click', onClick);
+export const createButton = ({ primary = false, size = 'medium', backgroundColor, label, onClick }) => {
+  const btn = document.createElement('button')
+  btn.type = 'button'
+  btn.innerText = label
+  btn.addEventListener('click', onClick)
 
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  btn.className = ['storybook-button', `storybook-button--${size}`, mode].join(' ');
+  const mode = primary ? '-primary' : '-secondary'
+  btn.className = ['Button', `-${size}`, mode].join(' ')
 
-  btn.style.backgroundColor = backgroundColor;
+  btn.style.backgroundColor = backgroundColor
 
-  return btn;
-};
+  return btn
+}
